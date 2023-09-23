1f4513b29c271d06252fa420dd9f0abee6703a35
Karan Shah <shahkaran281@gmail.com>
2023-09-23 14:04:35 -0700

    Time: 309 ms (83.33%), Space: 53.3 MB (74.69%) - LeetHub

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.py
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/1557-check-if-a-string-contains-all-binary-codes-of-size-k.py
@@ -1,22 +1,30 @@
 class Solution:
     def hasAllCodes(self, s: str, k: int) -> bool:
-        limit = pow(2,k)
-        val = 0
-        visited = set()
-        if len(s) < k:
-            return False
-        for i in range(k-1):
-            val = val*2 + int(s[i])
-        # print("Limit:", limit, "Val:",val)
-        i = k-1
-        while i < len(s):
-            val = val*2 + int(s[i])
-            # print(val)
-            if val < limit:
-                visited.add(val)
-            if len(visited) == limit:
-                return True
-            i+=1
-            val = val %(limit/2)
-        return False
+        return len(set(s[i : i + k] for i in range(len(s) - k + 1))) == 2**k
+
+
+
+
+
+
+
+
+
+        # limit = pow(2,k)
+        # val = 0
+        # visited = set()
+        # if len(s) < k:
+        #     return False
+        # for i in range(k-1):
+        #     val = val*2 + int(s[i])
+        # i = k-1
+        # while i < len(s):
+        #     val = val*2 + int(s[i])
+        #     if val < limit:
+        #         visited.add(val)
+        #     if len(visited) == limit:
+        #         return True
+        #     i+=1
+        #     val = val %(limit/2)
+        # return False
         
