ed33cba228c7646dd35a1a13d414e71c5194c887
Karan Shah <shahkaran281@gmail.com>
2023-10-12 15:11:53 -0700

    Time: 88 ms (84.31%), Space: 17.6 MB (28.59%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -0,0 +1,31 @@
+class Solution:
+    def asteroidCollision(self, a: List[int]) -> List[int]:
+        # [10,2,-5,-11,11]
+        # [-2,-2,1,-2]
+        stk = []
+
+        def fun(val):
+            while len(stk) != 0 and stk[-1]*val < 0:
+                if abs(stk[-1]) > abs(val):
+                    return
+                elif abs(stk[-1] == abs(val)):
+                    stk.pop()
+                    return
+                else:
+                    stk.pop()
+            stk.append(val)
+        i = 0
+        while i < len(a):
+            if len(stk) == 0:
+                stk.append(a[i])
+            else:
+                if stk[-1]*a[i] > 0:
+                    stk.append(a[i])
+                else:
+                    if stk[-1] < 0:
+                        stk.append(a[i])
+                    else:
+                        fun(a[i])
+            i += 1
+        return stk
+        
