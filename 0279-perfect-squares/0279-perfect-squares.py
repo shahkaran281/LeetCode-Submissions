d8ed3d8fd686e72164dac3f97b1a2d579c4db4b5
Karan Shah <shahkaran281@gmail.com>
2023-10-25 16:15:57 -0700

    Time: 2890 ms (53.43%), Space: 16.3 MB (68.28%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -6,7 +6,6 @@ class Solution:
         i = 1
         while i*i <= n:
             sqr.append(i*i)
-            dp[i*i] = 1
             i+=1
         for j in range(1, n + 1):
             for i in sqr:
