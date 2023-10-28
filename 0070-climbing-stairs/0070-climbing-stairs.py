88cb69d693d206400e851f401e106436db58a95d
Karan Shah <shahkaran281@gmail.com>
2023-10-28 15:02:57 -0700

    Time: 40 ms (41.51%), Space: 16.3 MB (40.9%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,15 +1,14 @@
 class Solution:
-    def __init__(self):
-        self.res = {}
-    def func(self,n:int) -> bool:
-        if n < 0:
-            return False
-        if n == 0:
-            return True
-        if n-1 not in self.res:
-            self.res[n-1] = self.func(n-1)
-        if n-2 not in self.res:
-            self.res[n-2] = self.func(n-2)
-        return self.res[n-1] + self.res[n-2]
     def climbStairs(self, n: int) -> int:
-        return self.func(n)
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            DP = [0] * (n+1)
+            DP[1] = 1
+            DP[2] = 2
+            for i in range(3,n+1):
+                DP[i] = DP[i-1]+DP[i-2]
+            return DP[-1]
+        
