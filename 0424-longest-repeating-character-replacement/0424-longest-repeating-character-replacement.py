41126e43f7066bc190d0a321cca8b0ff0ad50b7a
Karan Shah <shahkaran281@gmail.com>
2023-11-04 15:11:19 -0700

    Time: 218 ms (10.18%), Space: 16.3 MB (47.87%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,19 +3,14 @@ class Solution:
         res = 1
         i = 0
         j = 0
-        count = dict()
+        count = collections.Counter()
         while j < len(s):
-            if s[j] in count:
-                count[s[j]] +=1
-            else:
-                count[s[j]] = 1
+            count[s[j]] +=1
             while (j-i+1) - count[max(count, key=count.get)] > k:
                 count[s[i]]-=1
                 if count[s[i]] == 0:
-                    del count[s[i]]      
+                    del count[s[i]]  
                 i+=1
-            res = max(res,j - i+1)
-            print(count)
-            # print(f'j: {j} i:{i}')
+            res = max(res,j-i+1)
             j+=1
         return res
