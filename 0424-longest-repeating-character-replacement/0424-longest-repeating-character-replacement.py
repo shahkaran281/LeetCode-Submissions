37d66ad578501fb868f8a8eb902903b5173edb98
Karan Shah <shahkaran281@gmail.com>
2023-11-04 15:14:01 -0700

    Time: 111 ms (48.84%), Space: 16.3 MB (79.62%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -4,9 +4,11 @@ class Solution:
         i = 0
         j = 0
         count = collections.Counter()
+        maxCount = 0
         while j < len(s):
             count[s[j]] +=1
-            while (j-i+1) - count[max(count, key=count.get)] > k:
+            maxCount = max(maxCount,count[s[j]])
+            while (j-i+1) - maxCount > k:
                 count[s[i]]-=1
                 i+=1
             res = max(res,j-i+1)
