ad83f01f1d0d6e2bbf499d9620e3644d6eb0fd2a
Karan Shah <shahkaran281@gmail.com>
2023-11-08 13:51:34 -0800

    Time: 78 ms (85.12%), Space: 16.5 MB (69%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,35 +1,9 @@
 class Solution:
-    def searchInsert(self, nums: List[int], x: int,n:int) -> int:
-
-            if x <= nums[0]:
-                return 0
-            if x > nums[n-1]:
-                return n
-            l = 0
-            r = n-1
-            while l <= r:
-                mid = int((l+r)/2)
-                if nums[mid] == x:
-                    return mid
-                elif nums[mid] > x:
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            if nums[l] > x:
-                l-=1
-            return l+1
     def lengthOfLIS(self, nums: List[int]) -> int:
-        j = 1
-        for i in range(1,len(nums)):
-            k = self.searchInsert(nums,nums[i],j)
-            nums[k] = nums[i]
-            if k == j:
-                j+=1
-        return j
-
-
-
-        
-
-
+        n = 0
+        for i in range(len(nums)):
+            idx = bisect_left(nums[:n+1],nums[i])
+            nums[idx] = nums[i]
+            n = max(idx,n)
+        return n+1
         
