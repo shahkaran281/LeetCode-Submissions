055671324ff7a8499496576ca0c55508782a1c09
Karan Shah <shahkaran281@gmail.com>
2023-11-08 13:18:04 -0800

    Time: 263 ms (33.18%), Space: 20 MB (41.51%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        count = collections.Counter()
+        count = collections.Counter({0:1})
         res = 0
         currSum = 0
-        # for i in range(1,len(nums)):
-        #     nums[i] += nums[i-1]
         for i in range(len(nums)):
             currSum += nums[i]
-            if currSum == goal:
-                res+=1
             if currSum - goal in count:
                 res += count[currSum - goal]
             count[currSum] +=1
