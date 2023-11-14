c6d88dc6bb023b210e179e5b1c457b183a2c30da
Karan Shah <shahkaran281@gmail.com>
2023-11-13 17:59:20 -0800

    Time: 2169 ms (5.01%), Space: 17.1 MB (27.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -8,15 +8,10 @@ class Solution:
         j = 0
         res = "*" * (len(s)+1)
         while j <= len(s):
-            # print(f"i:{i} j:{j} curr: {curr} word: {s[i+1:j]}")
-            # print(tCount.items())
-            # print(curr.items())
-            # print(tCount.items() <= curr.items())
-            if  is_counter_subset(tCount, curr):
+
+            if  Counter(tCount) <= Counter(curr):
                 if len(s[i+1:j]) < len(res):
                     res = s[i+1:j]
-                # print('Got one: ',s[i+1:j])
-                # if 
                 i+=1
                 curr[s[i]] -=1
                 if curr[s[i]] == 0:
