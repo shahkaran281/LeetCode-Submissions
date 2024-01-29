6f488f3e4b99d9840baa03906b9eae2838ebef64
Karan Shah <shahkaran281@gmail.com>
2024-01-30 04:58:14 +0530

    Time: 134 ms (57.14%), Space: 17.2 MB (91.73%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -7,14 +7,9 @@ class Solution:
             if i - k >= 0:
                 if s[i-k] in vowels:
                     currRes -= 1
-                # print(f'LAST Position : {i-k} val: {s[i-k]} curr: {currRes}')
             if s[i] in vowels:
                 currRes +=1
-            res = max(res, currRes)
-            # print(f'i: {i} s[i]: {s[i]} curr : {currRes} last : {i-k}')
-
-
-                
+            res = max(res, currRes)              
         return res
         
         
