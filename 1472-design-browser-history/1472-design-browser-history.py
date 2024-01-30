9418576b9b5ae0e0c50220d06ff6de68f46c0681
Karan Shah <shahkaran281@gmail.com>
2024-01-31 01:17:08 +0530

    Time: 218 ms (35.60%), Space: 19 MB (91.30%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -8,27 +8,23 @@ class BrowserHistory:
     def __init__(self, homepage: str):
         self.curr = ListNode(homepage)
         
-
     def visit(self, url: str) -> None:
         nxt = ListNode(url)
         self.curr.next = nxt
         nxt.prev = self.curr
         self.curr = nxt 
         
-
     def back(self, steps: int) -> str:
         for _ in range(steps):
             if self.curr.prev:
                 self.curr = self.curr.prev
         return self.curr.val
         
-
     def forward(self, steps: int) -> str:
         for _ in range(steps):
             if self.curr.next:
                 self.curr = self.curr.next
         return self.curr.val
-        
 
 
 # Your BrowserHistory object will be instantiated and called as such:
