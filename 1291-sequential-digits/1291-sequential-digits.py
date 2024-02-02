cfe8d3c3280973f872cd466d6b261dc906e71545
Karan Shah <shahkaran281@gmail.com>
2024-02-02 19:06:46 +0530

    Time: 39 ms (40.37%), Space: 16.6 MB (53.11%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -5,19 +5,15 @@ class Solution:
             k = int(strX[0])
             for i in range(1,len(strX)):
                 k+=1
-                # print(f'k: {k}')
                 if k == 10:
-                    # print('X : {x} index : {i}')
                     return -1
                 strX = strX[:i] + str(k) + strX[i+1:]
             return int(strX)
         res = []
         base = pow(10,floor(log10(low)))
-        # print(f'Base : {base}')
         curr = base
         while curr <= high:
             val = fun(curr)
-            # print(f'Value : {curr} Res : {val} ')
             if val >= low and val <= high and val not in res:
                 res.append(val)
             if curr == 10 * base:
