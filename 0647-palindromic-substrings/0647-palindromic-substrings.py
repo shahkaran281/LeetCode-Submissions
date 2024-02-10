9cac1a0e9c5a1e6237012b46b7b4a578dd2713dd
Karan Shah <shahkaran281@gmail.com>
2024-02-10 10:39:32 +0530

    Time: 80 ms (71.04%), Space: 16.6 MB (65.40%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,15 +2,13 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
         def isPam(start,end):
-            # print(start,end)
             if start < 0 or end >= len(s):
                 return 0
             if s[start] != s[end]:
                 return 0
             return 1 + isPam(start-1,end+1)
         for i in range(len(s)):
-            count += isPam(i,i) 
-            count += isPam(i,i+1)
+            count += isPam(i,i) + isPam(i,i+1)
         
         return count
         
