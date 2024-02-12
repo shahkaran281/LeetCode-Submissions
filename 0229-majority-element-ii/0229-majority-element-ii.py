49a1eecfe9031094d0fad29b25c8531a9fb98b76
Karan Shah <shahkaran281@gmail.com>
2024-02-12 13:52:50 -0800

    Time: 98 ms (70.75%), Space: 17.8 MB (82.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,9 +1,23 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
-        res =  []
-        counter = collections.Counter(nums)
-        for num, count in counter.items():
-            if count > len(nums)/3:
-                res.append(num)
-        return res
-        
+  def majorityElement(self, nums: List[int]) -> List[int]:
+    ans1 = 0
+    ans2 = 1
+    count1 = 0
+    count2 = 0
+
+    for num in nums:
+      if num == ans1:
+        count1 += 1
+      elif num == ans2:
+        count2 += 1
+      elif count1 == 0:
+        ans1 = num
+        count1 = 1
+      elif count2 == 0:
+        ans2 = num
+        count2 = 1
+      else:
+        count1 -= 1
+        count2 -= 1
+
+    return [ans for ans in (ans1, ans2) if nums.count(ans) > len(nums) // 3]
