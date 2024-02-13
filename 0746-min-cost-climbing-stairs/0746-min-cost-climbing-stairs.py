2da2f3666aca980d4ab7f968f766cc3737038289
Karan Shah <shahkaran281@gmail.com>
2024-02-12 18:11:25 -0800

    Time: 53 ms (76.34%), Space: 16.7 MB (68.38%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -3,5 +3,4 @@ class Solution:
         dp = [0] * (len(cost)+1)
         for i in range(2,len(cost)+1):
             dp[i] = min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]) 
-        # print(dp)
         return dp[-1]
