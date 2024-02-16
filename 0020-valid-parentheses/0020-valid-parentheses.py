2de3df96f641ea1818e20538179d23208e34f236
Karan Shah <shahkaran281@gmail.com>
2024-02-16 15:37:54 -0800

    Time: 41 ms (28.59%), Space: 16.5 MB (85.33%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,7 +1,5 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) % 2 != 0:
-            return False
         close = {")":"(","}":"{","]":"["}
         start = {"(","{","["}
         stk = []
