294470524de4b2c83137b36715b0aa0b374faa8c
Karan Shah <shahkaran281@gmail.com>
2024-02-18 12:41:13 -0800

    Time: 420 ms (36.36%), Space: 30.6 MB (86.06%) - LeetHub

--- 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
+++ 1936-add-minimum-number-of-rungs/1936-add-minimum-number-of-rungs.py
@@ -3,8 +3,6 @@ class Solution:
         rungs.insert(0,0)
         count = 0
         for i in range(1,len(rungs)):
-            diff = rungs[i] - rungs[i-1]
-
-            count += ceil(diff/dist)-1
+            count += ceil((rungs[i] - rungs[i-1])/dist)-1
         return count
         
