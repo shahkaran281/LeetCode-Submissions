4825a5ca03992f5fd798b982f0242dc1d1ac8633
Karan Shah <shahkaran281@gmail.com>
2025-07-08 01:14:53 -0700

    Time: 1 ms (31.94%), Space: 18 MB (61.3%) - LeetHub

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def verticalTraversal(self, root: Optional['TreeNode']) -> List[List[int]]:
+        visited = defaultdict(list)
+
+        def dfs(node, x, y):
+            if not node:
+                return
+            visited[x].append((y, node.val))  # store y for sorting
+            dfs(node.left, x - 1, y + 1)
+            dfs(node.right, x + 1, y + 1)
+
+        dfs(root, 0, 0)
+
+        res = []
+        for x in sorted(visited.keys()):
+            # Sort by y, then by val
+            col_nodes = sorted(visited[x], key=lambda p: (p[0], p[1]))
+            res.append([val for y, val in col_nodes])
+        return res
